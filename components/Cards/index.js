adf8e2ffd65424316e01830ab70ea15e1f85a447
khashimovafiruza@gmail.com <khashimovafiruza@gmail.com>
2020-01-17 13:43:13 -0600

    Completed

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,44 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+ axios.get('https://lambda-times-backend.herokuapp.com/articles')
+ .then(response => {
+ //console.log(`THEN IS WORKING`, dataObj.data);
+ const articles = Object.values(response.data.articles);
+ console.log(articles);
+
+ articles.forEach(array => {
+  articles.forEach(article => {
+  const newCard = articleCreator(article);
+   articleEntryPoint.appendChild(newCard);
+ })
+})      
+ })
+ function articleCreator(article){
+    const newCard=document.createElement('div'),
+          newHeadDiv =document.createElement('div'),
+          newAutorDiv =document.createElement('div'),
+          imgDiv = document.createElement('div'),
+          autorImg = document.createElement('img'),
+          autorSpan = document.createElement('span');
+
+ //  // set class names
+          newCard.classList.add('card');
+          newHeadDiv.classList.add('headline');
+          newAutorDiv.classList.add('autor');
+          imgDiv.classList.add('img-container');
+
+          newCard.appendChild(newHeadDiv);
+          newCard.appendChild(newAutorDiv);
+          autorImg.appendChild(imgDiv);
+          newAutorDiv.appendChild(autorSpan);
+
+ // // set text content
+          newHeadDiv.textContent= article.headline;
+          autorImg.src = article.autorPhoto;
+          autorSpan.textContent = article.autorName;
+
+          return newCard;
+        }
+        const articleEntryPoint =document.querySelector('.cards-container');
